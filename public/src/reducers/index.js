978fca6b2c899eee377571a4f3dc6e759ec13919
Shruti Agarwal <shruti.agarwal@nutanix.com>
2020-02-12 11:44:41 +0530

    Removed unwanted code

--- public/src/reducers/index.js
+++ public/src/reducers/index.js
@@ -1,13 +1,7 @@
 import {combineReducers} from 'redux';
 
-import tweets from './tweetsReducer';
-import user from './userReducer';
-import projectDetails from './projectDetailsReducer';
 import loginReducer from './LoginReducer';
 
 export default combineReducers({
-  user,
-  tweets,
-  projectDetails,
   loginReducer
 });
