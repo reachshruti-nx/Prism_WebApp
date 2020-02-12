65d87aa45faeec1b1c3735db5490549c906899d3
Shruti Agarwal <shruti.agarwal@nutanix.com>
2020-02-12 18:13:16 +0530

    Base for alerts and tasks added

--- public/src/pages/Dashboard.js
+++ public/src/pages/Dashboard.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import Header from '../components/Header.js';
+import { Card } from 'antd';
+
 export default class Dashboard extends React.Component {
+
+  getClusterDetails = () => {
+    return (
+      <Card title="Default size card" extra={<a href="#">More</a>}>
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+    );
+  }
+
+  getTasks = () => {
+    return (
+      <Card title="Default size card" extra={<a href="#">More</a>}>
+        <p>Tasks</p>
+      </Card>
+    );
+  }
+
+  getAlerts = () => {
+    return (
+      <Card title="Default size card" extra={<a href="#">More</a>}>
+        <p>Tasks</p>
+      </Card>
+    );
+  }
 
   render () {
     return (
       <div>
         <Header/>
-        <div>
-          I am dashboard
+        <div class="dashboard-container">
+          { this.getClusterDetails() }
+          { this.getTasks() }
+          { this.getAlerts() }
         </div>
       </div>
     );
