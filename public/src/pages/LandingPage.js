ac543577fcf69d14e4ac4f84ddc64c3b0c499b53
Aamir Shah <aamir.shah@nutanix.com>
2020-02-12 23:15:57 +0530

    carousel + nodemon

--- public/src/pages/LandingPage.js
+++ public/src/pages/LandingPage.js
@@ -10,14 +10,48 @@ export default class LandingPage extends React.Component {
   render () {
     return (
       <div class="landing-page-container">
-        <Carousel autoplay>
-          <div> Welcome to Prism 1234 </div>
-          <div> Welcome to Prism 6789 </div>
-          <div> Welcome to Prism abcd </div>
-          <div> Welcome to Prism xvyz </div>
+        <Carousel autoplay={false}>
+          <div class="data">
+            <img src="/images/welcome.png" alt="background image" />
+
+            <div>
+              <h3>Welcome to Prism</h3>
+              <p>One App for managing your Enterprise Cloud.</p>
+            </div>
+           </div>
+
+          <div class="data">
+            <img src="/images/infra.png" alt="background image" />
+
+            <div>
+              <h3>Infrastructure Management</h3>
+              <p>Centraliszed management of physical and virtual infrastructure.</p>
+            </div>
+           </div>
+
+
+          <div class="data">
+            <img src="/images/insights.png" alt="background image" />
+
+            <div>
+              <h3>Operational Insights</h3>
+              <p>Insights and recommendations to guide your day to day operations.</p>
+            </div>
+           </div>
+
+
+          <div class="data">
+            <img src="/images/planning.png" alt="background image" />
+
+            <div>
+              <h3>Planning</h3>
+              <p>Insights to plan for the growth of your Enterprise cloud.</p>
+            </div>
+           </div>
+
         </Carousel>
         <div>
-          <Button 
+          <Button
             class="login-button"
             type="primary"
             onClick={() => this.getStarted()}>
